cafdb4ad92d7483445177a645883ea718d7eff41
taylor-young0 <59883690+taylor-young0@users.noreply.github.com>
2021-11-21 15:51:18 -0500

    Added floating search bar to HillsMapView

--- lib/views/bottom_navigation_bar/hills_map_view.dart
+++ lib/views/bottom_navigation_bar/hills_map_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 import 'package:tobogganapp/views/hill_info_bottom_sheet.dart';
 
 class HillsMapView extends StatefulWidget {
@@ -18,29 +19,34 @@ class _HillsMapViewState extends State<HillsMapView> {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-          center: otu,
-          zoom: 16.0,
-          onTap: (pos, latlong) {
-            // hide the bottom sheet if visible
-            if (Navigator.of(context).canPop()) {
-              // set state needed to toggle marker colour
-              setState(() {
-                _selectedMarkerIndex = -1;
-                Navigator.of(context).pop();
-              });
-            }
-          }),
-      layers: [
-        TileLayerOptions(
-          urlTemplate:
-              "https://api.mapbox.com/styles/v1/tayloryoung/ckw2deumz3jo614rtffqghrre/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGF5bG9yeW91bmciLCJhIjoiY2t3MjU0eWN4MGE5YjMxcHhsMjRpd3A0OSJ9.7UmX8FwS_dQQXNd5lgKQIA",
+    return Stack(
+      children: [
+        FlutterMap(
+          options: MapOptions(
+              center: otu,
+              zoom: 16.0,
+              onTap: (pos, latlong) {
+                // hide the bottom sheet if visible
+                if (Navigator.of(context).canPop()) {
+                  // set state needed to toggle marker colour
+                  setState(() {
+                    _selectedMarkerIndex = -1;
+                    Navigator.of(context).pop();
+                  });
+                }
+              }),
+          layers: [
+            TileLayerOptions(
+              urlTemplate:
+                  "https://api.mapbox.com/styles/v1/tayloryoung/ckw2deumz3jo614rtffqghrre/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGF5bG9yeW91bmciLCJhIjoiY2t3MjU0eWN4MGE5YjMxcHhsMjRpd3A0OSJ9.7UmX8FwS_dQQXNd5lgKQIA",
+            ),
+            MarkerLayerOptions(markers: [
+              getMarker(otu, 0, "OTU Park"),
+              getMarker(otu2, 1, "OTU Library Hill")
+            ])
+          ],
         ),
-        MarkerLayerOptions(markers: [
-          getMarker(otu, 0, "OTU Park"),
-          getMarker(otu2, 1, "OTU Library Hill")
-        ])
+        buildFloatingSearchBar()
       ],
     );
   }
@@ -83,6 +89,53 @@ class _HillsMapViewState extends State<HillsMapView> {
             });
           },
         );
+      },
+    );
+  }
+
+  // Modified from example at https://pub.dev/packages/material_floating_search_bar
+  Widget buildFloatingSearchBar() {
+    final FloatingSearchBarController controller =
+        FloatingSearchBarController();
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+      controller: controller,
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      automaticallyImplyBackButton: false,
+      onQueryChanged: (query) {
+        // Call your model, bloc, controller here.
+      },
+      onSubmitted: (search) {
+        print(search);
+        controller.close();
+      },
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return Container();
       },
     );
   }
