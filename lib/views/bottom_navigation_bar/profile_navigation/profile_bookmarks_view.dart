0e44168f11f679dbf83e0977da2a133b2554aaad
SJustinWong <71024087+SJustinWong@users.noreply.github.com>
2021-11-30 23:41:46 -0500

    Add files via upload
    Rough bookmark page with accurate data from the firestore database finished, will need further details on what to add

--- lib/views/bottom_navigation_bar/profile_navigation/profile_bookmarks_view.dart
+++ lib/views/bottom_navigation_bar/profile_navigation/profile_bookmarks_view.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class BookmarkView extends StatefulWidget {
@@ -8,13 +10,46 @@ class BookmarkView extends StatefulWidget {
 }
 
 class _BookmarkViewState extends State<BookmarkView> {
+  final bookmarks = FirebaseFirestore.instance.collection("user_data");
+  var currentUser = FirebaseAuth.instance.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Bookmarks"),
-        backgroundColor: Colors.blue,
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Bookmarks"),
+          backgroundColor: Colors.blue,
+        ),
+        body: StreamBuilder(
+          stream: bookmarks.doc(currentUser!.uid).snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data["bookmarks"].length,
+              itemBuilder: (BuildContext context, int index) {
+                final docData = snapshot.data["bookmarks"][index];
+
+                return Container(
+                  padding: const EdgeInsets.fromLTRB(7.5, 0, 7.5, 0),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        title: Text(docData.toString()),
+                        onTap: () {},
+                      ),
+                      const Divider(
+                        thickness: 1,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        ));
   }
 }
