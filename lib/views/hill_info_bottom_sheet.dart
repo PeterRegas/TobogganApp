10105a67893ac5d0cad548cd48c1a5cad98e44b6
taylor-young0 <59883690+taylor-young0@users.noreply.github.com>
2021-12-01 13:58:40 -0500

    HillsMapView now pulls hill markers from Firestore

--- lib/views/hill_info_bottom_sheet.dart
+++ lib/views/hill_info_bottom_sheet.dart
@@ -1,48 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:tobogganapp/model/hill.dart';
 
 class HillInfoBottomSheet extends StatelessWidget {
-  const HillInfoBottomSheet(this.name, {Key? key}) : super(key: key);
+  const HillInfoBottomSheet(this.hill, {Key? key}) : super(key: key);
 
-  final String name;
+  final Hill hill;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 120,
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            name,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          const SizedBox(height: 5),
-          Row(
-            children: [
-              Row(
-                children: const [
-                  Icon(Icons.star, color: Colors.amber),
-                  Icon(Icons.star, color: Colors.amber),
-                  Icon(Icons.star, color: Colors.amber),
-                  Icon(Icons.star, color: Colors.amber),
-                  Icon(Icons.star_half, color: Colors.amber)
-                ],
-              ),
-              const SizedBox(width: 10),
-              const Text("9 reviews",
-                  style: TextStyle(color: Colors.grey, fontSize: 18)),
-            ],
-          ),
-          const SizedBox(height: 5),
-          Row(
-            children: const [
-              Text("200 Simcoe St. North ⋅ 12km",
-                  style: TextStyle(color: Colors.grey, fontSize: 16))
-            ],
-          )
-        ],
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        constraints:
+            BoxConstraints(minWidth: MediaQuery.of(context).size.width),
+        height: 120,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              hill.name,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                starsForRating(hill.rating),
+                const SizedBox(width: 10),
+                Text(
+                    hill.reviews.length == 1
+                        ? "${hill.reviews.length} review"
+                        : "${hill.reviews.length} reviews",
+                    style: const TextStyle(color: Colors.grey, fontSize: 18)),
+              ],
+            ),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                Text("${hill.address} ⋅ 12km",
+                    style: const TextStyle(color: Colors.grey, fontSize: 16)),
+              ],
+            )
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget starsForRating(double rating) {
+    return Row(
+      children: const [
+        Icon(Icons.star, color: Colors.amber),
+        Icon(Icons.star, color: Colors.amber),
+        Icon(Icons.star, color: Colors.amber),
+        Icon(Icons.star_outline, color: Colors.amber),
+        Icon(Icons.star_half, color: Colors.amber)
+      ],
     );
   }
 }
