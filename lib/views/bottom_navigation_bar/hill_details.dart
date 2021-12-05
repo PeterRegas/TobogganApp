787f953558d33b99c64803a8efbfc6a70cc5a2f1
PeterRegas <55000513+PeterRegas@users.noreply.github.com>
2021-12-05 15:45:36 -0500

    fixfixfix

--- lib/views/bottom_navigation_bar/hill_details.dart
+++ lib/views/bottom_navigation_bar/hill_details.dart
@@ -240,7 +240,8 @@ class _AddEventState extends State<AddEvent> {
                         ),
                       ),
                       Container(
-                          height: 1000, //height of TabBarView
+                          height: MediaQuery.of(context).size.height /
+                              1.8, //height of TabBarView
                           decoration: BoxDecoration(
                               border: Border(
                                   top: BorderSide(
@@ -293,7 +294,7 @@ class _AddEventState extends State<AddEvent> {
                                       ])),
                                   Container(
                                     height: MediaQuery.of(context).size.height /
-                                        2.2,
+                                        2.65,
                                     child: Stack(
                                       alignment: AlignmentDirectional.bottomEnd,
                                       children: [
